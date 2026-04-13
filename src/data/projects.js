f7aeb0f8e683418a1cd6b7190b109b65e6844ea5
rafaelsamico <rsamicod@gmail.com>
2026-04-13 01:20:43 -0300

    feat(projects): preenche atributos e adiciona vibe hack

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,24 +2,35 @@ const projects = [
     {
         id: 1,
         name: "CInExt",
-        description: "<Descrição>",
-        objective: "<Objetivo>",
-        discipline: "CAD",
-        repoUrl: "https://github.com/...",
-        protoUrl: "https://loveable.com/...",
+        description: "O hub de Extensão do CIn-UFPE",
+        objective: "Solução para a comunidade extensionista do CIn-UFPE",
+        discipline: "CIN0131 - CAD",
+        repoUrl: "https://github.com/rafaelalinscin/cin-portal",
+        protoUrl: "https://cinext.lovable.app",
         stack: ["Loveable", "GitHub"],
-        learnings: "<Aprendizados>"
+        learnings: "Design de Produto, Design de UX/UI"
     },
     {
         id: 2,
         name: "Cadus",
-        description: "<Descrição>",
-        objective: "<Objetivo>",
-        discipline: "DESO",
-        repoUrl: "https://github.com/...",
-        protoUrl: "https://loveable.com/...",
+        description: "O pré-cadastro virtual da clínica digital da UFPE",
+        objective: "Solução para as clínicas-escola da UFPE",
+        discipline: "CIN0136 - DESO",
+        repoUrl: "https://github.com/bernardobelfort/cadus-facil-login",
+        protoUrl: "https://cadus-facil-login.lovable.app",
         stack: ["Loveable", "GitHub"],
-        learnings: "<Aprendizados>"
+        learnings: "Desenvolvimento de Software"
+    },
+    {
+        id: 3,
+        name: "Iris",
+        description: "O mapa de santuários da comunidade LGBTQIA+",
+        objective: "Solução para a comunidade LGBTQIA+ com IA",
+        discipline: "Vibe Hack ⭐ 1º lugar",
+        repoUrl: "https://github.com/AntonioCar0lin0/arco-iris-mapa",
+        protoUrl: "https://arco-iris-mapa,loveable.app",
+        stack: ["Claude", "Loveable", "GitHub"],
+        learnings: "Inteligência Artificial, Agentes"
     }
 ]
 
